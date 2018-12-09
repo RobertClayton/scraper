915cb4d3aa8820b1ce2f3812541d97f8a5ea9753
RobertClayton <robbie.c.clayton@gmail.com>
2018-12-09 01:15:18 +0000

    a simple scraper that pulls text off of a page

---
+++ lib/simple_scraper.rb
@@ -0,0 +1,11 @@
+endpoint = 'https://www.marvel.com/movies'
+doc = Nokogiri::HTML.parse(open(endpoint))
+
+content_wrapper = doc.css("div[class='content-grid grid-wrapper']")
+
+content_wrapper.css("div[class='card-body']").map do |film|
+  [
+    film.css("p[class='card-body__headline']").text,
+    film.css("p[class='card-footer__secondary-text']").text
+  ]
+end
