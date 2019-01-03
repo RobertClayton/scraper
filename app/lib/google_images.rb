2794af6f71ac41788d47bd072b0a0f4e87edf03c
RobertClayton <robbie.c.clayton@gmail.com>
2019-01-03 22:03:40 +0000

    using headless chrome

--- app/lib/google_images.rb
+++ app/lib/google_images.rb
@@ -1,12 +1,11 @@
 class GoogleImagesScraper
   def self.scrape(list)
-    browser = Watir::Browser.new(:chrome)
+    browser = Watir::Browser.new :chrome, headless: true
 
     scraped_images = list.map do |i|
       browser.goto('images.google.com')
       browser.text_field(title: 'Search').set i
       browser.send_keys :enter
-      sleep 1
       doc = Nokogiri::HTML.parse(browser.html)
       doc.at_css('div#ires img')['src']
     end
